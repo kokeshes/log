dd7c09011c9038e8d85d20e6a245a2d0a83d35b9
kokeshi <45554960+kokeshes@users.noreply.github.com>
2026-01-31 10:33:04 +0900

    Delete supabase.js

--- supabase.js
+++
@@ -1,8 +0,0 @@
-// docs/supabase.js
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
-
-// ★ここをあなたのSupabaseに差し替え
-export const SUPABASE_URL = "https://troequwqpdrpgadwfgbt.supabase.co";
-export const SUPABASE_ANON_KEY = "sb_publishable_XDbKzADmUoR1NnEPIRl57w_iB7r088Q";
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
