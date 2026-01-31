aef63b88e2e1509b8abbef1fc62108e9cd03d448
kokeshi <45554960+kokeshes@users.noreply.github.com>
2026-01-31 10:23:16 +0900

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,7 +2,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
 // ★ここをあなたのSupabaseに差し替え
-export const SUPABASE_URL = "https://YOUR_PROJECT.supabase.co";
-export const SUPABASE_ANON_KEY = "YOUR_ANON_PUBLIC_KEY";
+export const SUPABASE_URL = "https://troequwqpdrpgadwfgbt.supabase.co";
+export const SUPABASE_ANON_KEY = "sb_publishable_XDbKzADmUoR1NnEPIRl57w_iB7r088Q";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
